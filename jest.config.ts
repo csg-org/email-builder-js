e7a2294cb6ab8e619b9d1bf117818de288c0b914
Justin Frahm <justin@inspiringapps.com>
2025-09-23 17:19:11 -0600

    Replace insane with DOMPurify, bump alphas

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,12 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverageFrom: [
+    'packages/*/src/**/*.{ts,tsx}',
+    '!packages/*/src/**/*.spec.{ts,tsx}',
+    '!packages/*/src/**/*.test.{ts,tsx}',
+  ],
 };
 
 export default config;
