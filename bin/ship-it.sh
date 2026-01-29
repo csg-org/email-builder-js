d791f91dd2a2174e83df643c97dad8e567c9957a
Joshua Kravitz <jlkravitz@gmail.com>
2026-01-29 14:27:57 -0600

    Upgrade to non-alpha version

--- bin/ship-it.sh
+++ bin/ship-it.sh
@@ -1,42 +1,34 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 
 for package in $(find packages -name 'block-*' -type d -d 1); do
   (
+    set -e
     echo "Deploying $package"
     cd "$package"
     npm install
     npm run build
-  )
-done
-
-for package in $(find packages -name 'block-*' -type d -d 1); do
-  (
-    echo "Deploying $package"
-    cd "$package"
     npm publish --access public --tag latest
   )
 done
 
 (
+  set -e
+  echo "Deploying document-core"
   cd packages/document-core
   npm install
   npm run build
-)
-
-(
-  cd packages/document-core
   npm publish --access public --tag latest
 )
 
+echo "It may take several minutes for the subpackages to appear in the registry."
+read -p "Press enter to continue"
+
 (
+  set -e
+  echo "Deploying email-builder"
   cd packages/email-builder
   npm install
   npm run build
-)
-
-(
-  cd packages/email-builder
   npm publish --access public --tag latest
 )
-
