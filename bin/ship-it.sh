52dd5e398f8820d6582a13c5072cb038b84c1845
Joshua Kravitz <jlkravitz@gmail.com>
2026-01-29 13:30:32 -0600

    Updates to ship-it script and versioning

--- bin/ship-it.sh
+++ bin/ship-it.sh
@@ -5,15 +5,38 @@ for package in $(find packages -name 'block-*' -type d -d 1); do
   (
     echo "Deploying $package"
     cd "$package"
+    npm install
+    npm run build
+  )
+done
+
+for package in $(find packages -name 'block-*' -type d -d 1); do
+  (
+    echo "Deploying $package"
+    cd "$package"
     npm publish --access public --tag latest
   )
 done
 
 (
   cd packages/document-core
+  npm install
+  npm run build
+)
+
+(
+  cd packages/document-core
   npm publish --access public --tag latest
 )
+
+(
+  cd packages/email-builder
+  npm install
+  npm run build
+)
+
 (
   cd packages/email-builder
   npm publish --access public --tag latest
 )
+
